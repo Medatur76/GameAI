Fixed some incorrect values and started trying to send inputs to Trackmania

Trackmania/AI/ActivationClasses/BinaryStep.py
@@ -1,10 +1,11 @@
 from ActivationClasses.Activation import Activation
+import numpy as np
 
 class BinaryStepActivation(Activation):
-    def forward(self, inputs):
-        if len(inputs) == 1:
+    def forward(inputs):
+        if isinstance(inputs, np.float64) or len(inputs) == 1:
             output = 0
-            if i > 0: output = 1
+            if inputs > 0: output = 1
             return output
         else:
             outputs: list[int] = []

Trackmania/AI/NeuralClasses/NeuralLayer.py
@@ -2,7 +2,7 @@ from ActivationClasses.Activation import Activation
 import numpy as np
 
 class NeuralLayer():
-    def __init__(self, n_inputs: int, n_outputs: int, activation: Activation, activations: list[Activation]=None, weights: list[list[float]] = None, biases: list[list[float]] = None):
+    def __init__(self, n_inputs: int, n_outputs: int, activation: Activation, activations: list[Activation]=None):
         self.multiActivations = False
         self.activation = activation
         self.activations = activations
@@ -11,10 +11,8 @@ class NeuralLayer():
                 self.multiActivations = True
             else:
                 raise ValueError("Mismatch between requested outputs ("+ str(n_outputs) + ") and requested activations (" + str(len(activations)) + ")")
-        if weights == None: self.weights = 0.1 * np.random.randn(n_inputs, n_outputs)
-        else: self.weights = weights
-        if biases == None: self.biases = np.random.randn(1, n_outputs)
-        else: self.biases = biases
+        self.weights = 0.1 * np.random.randn(n_inputs, n_outputs)
+        self.biases = np.random.randn(1, n_outputs)
         self.nInputs = n_inputs
         self.nOutputs = n_outputs
     def forward(self, inputs, use_activation=True):
@@ -22,14 +20,14 @@ class NeuralLayer():
             if not self.multiActivations: self.output = self.activation.forward(np.dot(inputs, self.weights) + self.biases)
             else:
                 outputs = []
-                for o in range(len(self.biases)):
-                    output = self.biases[o]
-                    for w in range(len(self.weights[o])):
-                        output += self.weights[0][w]*inputs[w]
+                for o in range(len(self.biases[0])):
+                    output = self.biases[0][o]
+                    for w in range(self.nInputs):
+                        output += self.weights[w][o]*inputs[0][w]
                     outputs.append(self.activations[o].forward(output))
                 self.output = outputs
 
         else: self.output = np.dot(inputs, self.weights) + self.biases
     def train(self, m: float = 0.05):
         self.weights += m * np.random.randn(self.nInputs, self.nOutputs)
-        self.output += m * np.random(1, self.nOutputs)
+        self.biases += m * np.random.randn(1, self.nOutputs)